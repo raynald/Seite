17dd78df0a2460f5d32e3ecf97cfcb3e9c629dd8
Raynald Chung <raynaldchung@gmail.com>
2015-06-13 18:23:23 +0200

    place a bid

--- auciton_server.py
+++ auciton_server.py
@@ -23,10 +23,10 @@ def room():
     Render the auction room page
     """
     _table = auction.GetAll()
-    table = list()
-    for id in _table:
-        item = auction.GetByID(id)
-        table.append(item)
+    table = list(_table)
+    # for id in _table:
+    #      item = my_auc.GetByID(id)
+    #      table.append(item)
     return render_template('auction_list.html', table=table)
 
 @app.route('/single_item/<item_id>')
@@ -35,7 +35,30 @@ def show(item_id):
     Given the user the detailed info of an item
     """
     item = auction.GetByID(item_id)
-    return render_template('single_item.html', item_id=str(item_id), item=item)
+    return render_template('single_item.html', item=item)
+
+@app.route('/update_bid/<item_id>,<new_price>')
+def update(item_id, new_price):
+    """
+    Update the price of that product
+    """
+    auction.PlaceABid(item_id, new_price)
+    item = auction.GetByID(item_id)
+    return render_template('update_bid.html', item=item, new_price=new_price)
+
+@app.route('/news')
+def display():
+    """
+    Give the bidders the recent news
+    """
+    return render_template('news.html')
+
+@app.route('/about_us')
+def contact():
+    """
+    Contact US
+    """
+    return render_template('about_us.html')
 
 if __name__ == '__main__':
     app.run()
--- auction.py
+++ auction.py
@@ -2,21 +2,33 @@ import json
 import random
 import pymongo as mg
 
-coll = mg.MongoClient()['migros']['lebensmittel']
-item_num = 292
-
 def GetAll():
     """
     image, price, name
     """
+    coll = mg.MongoClient()['migros']['lebensmittel']
+    item_num = 291
     hasImage = coll.find({'info.image.medium':{'$exists':True}})
     _table = list()
     for iter in xrange(item_num):
         item = hasImage.next()
-        item.update({'price': round(random.uniform(2, 50), 2)})
-        _table.append(item['id'])
+        # item.update({'price': round(random.uniform(2, 50), 2)})
+        info = dict()
+        info["id"] = item["id"]
+        info["image"] = item['info']['image']
+        info['price'] = item['health']
+        info['description'] = "This is a real description...."
+        info['name'] = item['name']
+        _table.append(info)
     return _table
-def GetByID(id):
-    return coll.find_one({'id':id})
 
-# if __name__ == "__main__":
+def GetByID(id):
+    col = mg.MongoClient()['migros']['lebensmittel']
+    cursor = col.find({'id': int(id)})
+    for item in cursor:
+        return item
+
+def PlaceABid(id, price):
+    col = mg.MongoClient()['migros']['lebensmittel']
+    cursor = col.update({"id": int(id)}, {'$set': {'health': price}}, False)
+    print cursor
